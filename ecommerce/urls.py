9cbb584a7c9bbcf9aa0788ddc91e7ff2c9fad474
Femy Stephen <femystephen16@gmail.com>
2018-11-20 11:47:08 +0000

    product details added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from accounts.views import signup,show_profile
-from products.views import product_list
+from products.views import product_list,product_details
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,6 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/signup/', signup, name='signup'),
     path('accounts/profile', show_profile, name='show_profile'),
-    
+    path('product_details/<int:id>',product_details,name='product_details'),
+    path('media/<path:path>',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def product_list(request):
-    return render(request,"products/product_list.html")
+    product=Product.objects.all()
+    return render(request,"products/product_list.html",{'product':product})
+    
+def product_details(request,id):
+    product=Product.objects.get(pk=id)
+    return render(request, "products/product_details.html",{'product':product})
